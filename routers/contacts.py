b8b86a3fcb80ea1ef7b7e6a2c3b66c79416ce4a5
wneusa2401 <wneusa2401@gmail.com>
2022-10-26 21:58:41 -0500

    Proyecto Saldo

--- routers/contacts.py
+++ routers/contacts.py
@@ -9,19 +9,43 @@ contacts = Blueprint('contacts',__name__)
 def index():
     return render_template('index.html')
 
-@contacts.route('/new', methods=['POST'])
+@contacts.route('/new')
 def new_contact():
+        return render_template('commit.html')
+
+@contacts.route("/register")
+def register_contact():
     
-        nombre = request.form['nombre']
-        money = request.form['money']
+        #nombre = request.form['nombre']
+        #nombre1 = request.form['nombre1']
+        #Apellido = request.form['Apellido']
+        #Apellido1 = request.form['Apellido1']
+        #email = request.form['email']
+        #phone = request.form['phone']
         
-        new_contact = Contact(nombre, money)
+        #new_contact = Contact(nombre,nombre1,Apellido,Apellido1,email,phone)
 
-        db.session.add(new_contact)
-        db.session.commit()
+        #db.session.add(register_contact)
+        #db.session.commit()
+        
+    return render_template('register.html')
 
-        return "SALVO INFO"
+@contacts.route("/saldo")
+def saldo_contact():
+    return render_template('saldo.html')
 
-@contacts.route("/update")
+@contacts.route("/password")
+def password_contact():
+    
+        #correo = request.form['correo']
+        
+        #new_contact = Contact(correo)
+
+        #db.session.add(register_contact)
+        #db.session.commit()
+    
+    return render_template('password.html')
+
+@contacts.route("/login")
 def update_contact():
-    return "saldo Actualizado guardado"
+    return render_template('commit.html')
